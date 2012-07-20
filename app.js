a01881e9c8635f6b79097ccba107ff80d1aaa93a
Craig Hammell <cilphex@gmail.com>
2012-07-20 00:15:18 -0400

    initial commit, copied from c9

---
+++ app.js
@@ -0,0 +1,58 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    stylus = require('stylus'),
+    router = require('./config/router.js');
+  
+  //, routes = require('./routes')
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+  
+    // ... your middleware here
+    app.use(stylus.middleware({
+        src: __dirname + '/views',
+        dest: __dirname + '/public',
+        compile: function(str, path) {      // optional, but recommended
+        	return stylus(str)
+        		.set('filename', path)
+        		.set('warn', true)
+        		.set('compress', true);
+    	}
+    }));
+  
+    app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler()); 
+});
+
+app.register('.pjs', require('pubjs'));
+
+// Routes - automatically uses ./routes/index.js unless other routes file specified
+
+router.setupRoutes(app);
+
+//app.get('/', routes.index);
+//app.get('/test', routes.test);
+
+//app.listen(3000);
+// Change by CH 1/27/2012
+app.listen(process.env.PORT);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ config/router.js
@@ -0,0 +1,30 @@
+// Added by Craig 2/6/2012
+
+var routes = require('../routes');
+
+var paths_to_routes = {
+    '/': routes.pad,
+    '/alt': routes.alt,
+    '/about': routes.about,
+    
+    // Sandbox
+    '/sandbox': routes.sandbox,
+    '/sandbox/:path': routes.sandbox
+    
+    /*,
+    '/sandbox/*': routes.error_404,
+    
+    // Error routes - keep last
+    '/500': routes.error_500,
+    '/*': routes.error_404
+    */
+};
+
+exports.setupRoutes = function(app) {
+    // Setup regular routes
+    for (var path in paths_to_routes) {
+        app.get(path, paths_to_routes[path]);
+    }
+    // Setup errors
+    app.error(routes.error);
+};
